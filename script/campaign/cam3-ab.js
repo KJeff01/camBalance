e4574c388c76d3bfabd82debe2d60f913182bc67
KJeff01 <kjeff1997@gmail.com>
2023-11-08 00:29:46 -0600

    Move Plasteel upgrades around

--- script/campaign/cam3-ab.js
+++ script/campaign/cam3-ab.js
@@ -284,7 +284,6 @@ function eventStartLevel()
 	camCompleteRequiredResearch(mis_nexusRes, CAM_NEXUS);
 
 	enableResearch("R-Sys-Resistance-Upgrade01", CAM_HUMAN_PLAYER);
-	enableResearch("R-Defense-WallUpgrade10", CAM_HUMAN_PLAYER);
 	winFlag = false;
 	hackFailChance = (difficulty <= EASY) ? 40 : 30;
 
